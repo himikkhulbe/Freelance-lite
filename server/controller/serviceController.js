71183a970a262ae834a947e9a6110774e3b6721a
Md Alkama <mdalkamadad@gmail.com>
2025-06-21 23:54:20 +0530

    freelancer route and services controller added except update controller

--- server/controller/serviceController.js
+++ server/controller/serviceController.js
@@ -1,5 +1,6 @@
-import userModel from "../models/userModel.js";
-import serviceModel from "../models/freelancerModels.js";
+import mongoose from "mongoose";
+import User from "../models/userModel.js";
+import Service from "../models/freelancerModels.js";
 
 export const uploadServices = async (req, res) => {
     try {
@@ -19,7 +20,7 @@ export const uploadServices = async (req, res) => {
 
         const userId = req.user._id;
 
-        const user = await userModel.findById(userId);
+        const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
@@ -28,7 +29,7 @@ export const uploadServices = async (req, res) => {
             return res.status(403).json({ message: "Only freelancers can upload services." });
         }
 
-        const service = await serviceModel.create({
+        const service = await Service.create({
             user: userId,
             title,
             description,
@@ -52,10 +53,34 @@ export const uploadServices = async (req, res) => {
     }
 };
 
+
+export const getMyServices = async (req, res) => {
+    // Get the user id from the loggedin user
+    const userId = req.user._id;
+    try {
+        // Find the user in the database
+        const user = await User.findById(userId);
+        // If the user is not found, return a 404 status code with a message
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        // Find the services associated with the user
+        const services =  await Service.find({ user: userId });
+        // Return the services with a 200 status code
+        res.status(200).json(services);
+    }catch(error){
+        // If there is an error, return a 500 status code with a message
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
 export const getServices = async (req, res) => {
     const userId = req.params.id;
     console.log(userId);
     try {
+        if(!mongoose.Types.ObjectId.isValid(userId)){
+            return res.status(404).json({ message: "Invalid User Id" });
+        }
         const user = await User.findById(userId);
         if(!user){
             return res.status(404).json({ message: "User not found" });        
@@ -65,4 +90,41 @@ export const getServices = async (req, res) => {
     }catch(error){
         res.status(500).json({ message: "Internal server error" });
     }
+}
+
+export const getservice = async (req, res) => {
+    const serviceId = req.params.id;
+    try {
+        if(!mongoose.Types.ObjectId.isValid(serviceId)){
+            return res.status(404).json({ message: "Invalid Service Id" });
+        }
+        const service = await Service.findById(serviceId);
+        if(!service){
+            return res.status(404).json({ message: "Service not found" });        
+        }
+        res.status(200).json(service);
+    }catch(error){
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const deleteService = async (req, res) => {
+    const serviceId = req.params.id;
+    const userId = req.user._id;
+    try {
+        if(!mongoose.Types.ObjectId.isValid(serviceId)){
+            return res.status(404).json({ message: "Invalid Service Id" });
+        }
+        const service = await Service.findById(serviceId);
+        if(!service){
+            return res.status(404).json({ message: "Service not found" });        
+        }
+        if(service.user.toString() !== userId.toString()){
+            return res.status(403).json({ message: "You are not authorized to delete this service" });
+        }
+        await Service.findByIdAndDelete(serviceId);
+        res.status(200).json({ message: "Service deleted successfully" });
+    }catch(error){
+        res.status(500).json({ message: "Internal server error" });
+        }
 }
--- server/routes/freelancerRoutes.js
+++ server/routes/freelancerRoutes.js
@@ -1,11 +1,20 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import {uploadServices, getServices} from "../controller/serviceController.js";
+import {uploadServices, getMyServices, getServices, getservice, deleteService} from "../controller/serviceController.js";
 
 const router = express.Router();
 
 router.post("/service", protect, uploadServices);
-router.get("/:id/service", getServices);
+router.get("/services/my", protect, getMyServices);
+router.get("/services/:id", getServices);
+router.get("/service/:id", getservice);
+// router.put("/service/:id", protect, updateService);
+router.delete("/service/:id", protect, deleteService);
+
+
+
+
+
 
 
 export default router;
