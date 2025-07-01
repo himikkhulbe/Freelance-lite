013413700b6fc60efd9b75549f0febf93130bd59
Md Alkama <mdalkamadad@gmail.com>
2025-07-01 23:01:10 +0530

    added bottom space in rating section

--- client/src/components/Profile/components/RatingSection/RatingSection.jsx
+++ client/src/components/Profile/components/RatingSection/RatingSection.jsx
@@ -3,7 +3,7 @@ import RatingCard from './components/RatingCard/RatingCard';
 
 function RatingSection({ user, renderStars, formatDate, open }) {
     return (
-        <div className="w-full min-h-[120px] p-[30px] border-[0.5px] border-slate-500 bg-slate-50 rounded-lg">
+        <div className="w-full min-h-[120px] pt-[30px] px-[30px] border-[0.5px] border-slate-500 bg-slate-50 rounded-lg">
             <div className="flex justify-between items-center grow">
                 <p className="text-black text-lg font-semibold">Recent Reviews</p>
                 {user?.ratings?.length > 3 &&
@@ -14,7 +14,7 @@ function RatingSection({ user, renderStars, formatDate, open }) {
             </div>
             <div className="mt-[20px]">
                 {user?.ratings?.length === 0 ? (
-                    <p className="text-gray-500 text-sm font-semibold">No rating found</p>
+                    <p className="text-gray-500 text-sm font-semibold mb-[30px]">No rating found</p>
                 ) : (
                     user?.ratings?.slice(0, 3).map((rating) => (
                         <RatingCard
