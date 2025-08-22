5bf694794535f5d0bfe477879d6551bff7cfe431
Md Alkama <mdalkamadad@gmail.com>
2025-08-22 17:32:53 +0530

    fixed login cookie setting

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -58,7 +58,8 @@ export const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production", 
-            sameSite: "none",
+            sameSite: "None",
+            path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000, 
         });
 
