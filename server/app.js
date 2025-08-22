07ac666704d8de946892814e7e6b4eceea6161c8
Md Alkama <mdalkamadad@gmail.com>
2025-08-22 17:08:13 +0530

    update cors policy

--- server/app.js
+++ server/app.js
@@ -14,7 +14,8 @@ import clientRoutes from "./routes/clientRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173",
+    "freelancer-lite.vercel.app"], credentials: true }));
 
 app.use(cookieParser());
 // Allow credentials and allow requests from your frontend's origin
